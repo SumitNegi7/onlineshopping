d93bcc2fe9631a8a9bd3b8b81d30e84575b19067
sumit <sumitnegi2142@gmail.com>
2020-09-11 18:01:28 +0530

    up  nav

--- frontend1/src/App.js
+++ frontend1/src/App.js
@@ -22,6 +22,16 @@ import ProfileScreen from "./screens/ProfileScreen";
 import Footer from "./footer";
 import Elanding from "./screens/Elanding";
 function App(props) {
+
+
+  const closelink = () => {
+    const header1 = document.querySelector('.navbar');
+    const closelinks = document.querySelector('.close-tab');
+    const handburger1 = document.querySelector('.handburger');
+    header1.classList.toggle('navbar-active');
+    handburger1.classList.toggle('toggle');
+    closelinks.classList.toggle('close-tab-active');
+  }
   const OnNavSlideClick = () => {
     const header1 = document.querySelector(".navbar");
     const handburger1 = document.querySelector(".handburger");
@@ -29,15 +39,15 @@ function App(props) {
 
     header1.classList.toggle("navbar-active");
     handburger1.classList.toggle("toggle");
-    navlinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navlinkfade 0.5s ease forwards ${
-          index / 7 + 0.3
-          }s`;
-      }
-    });
+    // navlinks.forEach((link, index) => {
+    //   if (link.style.animation) {
+    //     link.style.animation = "";
+    //   } else {
+    //     link.style.animation = `navlinkfade 0.5s ease forwards ${
+    //       index / 7 + 0.3
+    //       }s`;
+    //   }
+    // });
   };
   // const drop = () => {
   //   const box = document.querySelector('.dropdown-content');
@@ -66,44 +76,53 @@ function App(props) {
       <div className="App1">
         <div className="navbar-container">
           <nav>
-            <div class="outer">
-              <div className="handburger" onClick={OnNavSlideClick}>
+            <div className="outer">
+              <div className="handburger" onClick={OnNavSlideClick}  >
                 <div className="line1"></div>
                 <div className="line2"></div>
                 <div className="line3"></div>
               </div>
-              <Link to="/">
-                <div class="logobox">PratapInfotech</div>
+              <Link to="/">   <div className="logobox">
+                Pratapinfotech
+        </div>
               </Link>
-              <ul class="navbar ">
-                <div class="dropdown" >
-                  <span>
-                    <Link to="/product">
-                      <li className="none one">
-                        Shop
-                        <i class="fa fa-caret-down"></i>
-                      </li>
-                    </Link>
-                  </span>
-                  <div class="dropdown-content">
-                    <Link to="/category/Desktops">
-                      <li className="drop">Desktops</li>
-                    </Link>
-                    <Link to="/category/Laptops">
-                      <li className="drop">Laptop</li>
-                    </Link>
-                    <Link to="/category/Accessories">
-                      <li className="drop">Accessories</li>
-                    </Link>
-                    <Link to="/category/Refubrished">
-                      <li className="drop">Refubrished</li>
-                    </Link>
+              <ul className="navbar" >
+
+                <div className="dropdown" >
+                  <li className="one">Shop
+          <i className="fa fa-caret-down"></i>
+                  </li>
+                  <div className="dropdown-content">
+                    <Link to="/category/Desktops">  <li className="drop">Desktop</li></Link>
+                    <Link to="/category/Laptops">  <li className="drop">Laptop</li></Link>
+                    <Link to="/category/Accessories">  <li className="drop">Accessories</li></Link>
+                    <Link to="/category/Refubrished">    <li className="drop">Refurbished</li></Link>
                   </div>
                 </div>
-                <li class="none one">Visit Store</li>
-                <li class="none one">Services</li>
-                <li class="none one">Contact</li>
+                <Link to='/product'>
+                  <li className="newshop one close-tab" onClick={closelink}> Shop</li>
+                </Link>
+                <Link to='/Visit'>
+                  <li className=" one " onClick={closelink}>Visit Store</li>
+                </Link>
+                <Link>
+                  <li className=" one " onClick={closelink}>Services</li>
+                </Link>
+                <Link>
+                  <li className="one" onClick={closelink}>Contact</li>
+                </Link>
+                <div className="copy-right">
+                  <p className="pcopy">
+                    2020 @ Pratapinfotech
+            </p>
+                  <p className="siteby">
+                    Site By Badboys
+            </p>
+
+                </div>
+
               </ul>
+
               <ul class=" navbar1">
                 {userInfo ? (
                   <Link to="/profile">{userInfo.name} </Link>
@@ -123,6 +142,8 @@ function App(props) {
                 </Link>
               </ul>
             </div>
+
+
           </nav>
         </div>
         {/* 
